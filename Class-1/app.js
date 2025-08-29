4ef6894be534a841039ab91d7d30df6ebdad3017
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-08-29 08:05:47 +0500

    This is my 1st class of JS

---
+++ Class-1/app.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+console.log("Welcome to JavaScript programming!");
+
+// ways of making variables
+var name = "Ali";
+let age = 25;
+const country = "Pakistan";
+console.log(name);
+console.log(cars);
+console.log(date);  
+

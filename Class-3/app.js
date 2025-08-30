831d3e5b4f3d7924a5799576f8891020218d1a11
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-08-31 00:47:31 +0500

    This is my 3rd class of JS

---
+++ Class-3/app.js
@@ -0,0 +1,34 @@
+// alert("hello");
+// let fName = prompt("what is your name");
+// console.log(fName);
+
+
+
+//===================== DATA TYPE =====================
+
+
+// Number
+
+var number1 = 10
+console.log(number1) //Let 10 be the number
+ //Booleans
+
+ var boolean = "true & false";
+ console.log(boolean); // Let true & false be the number
+ // String
+ var string = "Ali"
+ console.log(string); //let Ali be the string
+ // Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25"); 
+
+
+
+
+
+

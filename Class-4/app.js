7e99b2c0d7d93eb5d35ecde305af68e365b60130
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-09-01 02:29:12 +0500

    This is my 4th class of JS

---
+++ Class-4/app.js
@@ -0,0 +1,41 @@
+// =================Arithmatic operations==================
+// ===ADD===
+
+// WITH STRING
+var a = 10;
+var b = "20";
+// in string
+console.log(a+b);
+
+
+// WITHOUT SRTRING
+var num1 = 10;
+var num2 = 20;
+// convert in number so
+Number(num1)
+Number(num2)
+
+console.log(num1+num2);
+
+// === sub ===
+// It's Same with string or with out string
+console.log(num2 - num1);
+
+//===== multiply =======
+
+// It's Same with string or with out string
+console.log(num1*num2);
+
+//=============== divide =========
+
+// It's Same with string or with out string
+console.log(num2/num1)
+
+//=====reminder========
+console.log(2%4);
+
+
+
+
+
+

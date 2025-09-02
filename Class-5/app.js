14acfbc874f66d7a33d5a9cdca667a7096b15945
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-09-02 10:26:48 +0500

    This is my 5th class of JS

---
+++ Class-5/app.js
@@ -0,0 +1,36 @@
+// =============incremenentOperator============
+//post incremenent
+let x = 1;
+console.log(x);
+x++;
+console.log(x);
+//pre incrememnent
+let y = 1;
+console.log(++x);
+x+=2;
+console.log(x);
+// ==============deceremenetOperator========
+//post incremenent
+
+let a = 2;
+console.log(a--);
+console.log(a)
+//pre incrememnent
+
+let b = 3;
+console.log(b)
+
+console.log(--b)
+
+// SOLVE this
+
+let c = 5;
+console.log(c++ + ++c + c++ + c++ - --c + c-- + ++c);
+
+
+
+
+
+
+
+

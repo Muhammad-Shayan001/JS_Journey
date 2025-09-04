7d46c5c71631bb5704254033f13ecab31fbd3f59
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-09-05 03:40:08 +0500

    This is my 7th class of JS

--- Class-7/app.js
+++ Class-7/app.js
@@ -1,42 +1,42 @@
 // =============Arrays=========
-// let personalInfo = ["Jhon" , "18" , "student" , "single"]
+let personalInfo = ["Jhon" , "18" , "student" , "single"]
 
-// console.log(personalInfo);
-// console.log([3]);
-// console.log([0]);
-// console.log([1]);
+console.log(personalInfo);
+console.log([3]);
+console.log([0]);
+console.log([1]);
 
-// const fruits = ["Mango" , "Apple" , "Banana","Graps" , "Tomato" , "Kivi"];
+const fruits = ["Mango" , "Apple" , "Banana","Graps" , "Tomato" , "Kivi"];
 
-// console.log(fruits)
-// console.log(fruits.length)
+console.log(fruits)
+console.log(fruits.length)
 
 
-// alert(fruits[0]);
-// alert(fruits[3]);
-// alert(fruits[5]);
-// console.log(fruits.pop())
-// fruits.pop()
-// console.log(fruits)
-// fruits.push("water melon", "peach")
-// console.log(fruits)
-// fruits.shift()
-// console.log(fruits)
+alert(fruits[0]);
+alert(fruits[3]);
+alert(fruits[5]);
+console.log(fruits.pop())
+fruits.pop()
+console.log(fruits)
+fruits.push("water melon", "peach")
+console.log(fruits)
+fruits.shift()
+console.log(fruits)
 
-// fruits.unshift("pomegrand" , "melon")
+fruits.unshift("pomegrand" , "melon")
 
-// console.log(fruits)
+console.log(fruits)
 
-// fruits.pop()
-// fruits.pop()
-// fruits.pop()
-// fruits.pop()
-// console.log(fruits)
+fruits.pop()
+fruits.pop()
+fruits.pop()
+fruits.pop()
+console.log(fruits)
 
-// fruits.shift()
-// fruits.shift()
-// fruits.shift()
-// fruits.shift()
-// console.log(fruits)
+fruits.shift()
+fruits.shift()
+fruits.shift()
+fruits.shift()
+console.log(fruits)
 
 

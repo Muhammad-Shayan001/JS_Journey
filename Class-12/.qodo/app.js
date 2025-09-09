d777cea0e61138a11c59f20a3e7ebd27b3e3f885
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-09-10 02:44:18 +0500

    This is my 12th class of JS

---
+++ Class-12/.qodo/app.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < 10 ; i++) {
+   console.log(i);
+   
+    
+}
+
+const fruitesName = ['Apple' , ' Mango' , 'Banana' , 'Graps' ]
+
+for (let i = 0; i < fruitesName.length; i++) {
+    
+    const element = fruitesName[i];
+    
+    console.log(element);
+    
+
+
+}

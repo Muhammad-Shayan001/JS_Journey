6ed5137c2bb79902c3b7c4a3ab00c5f7790ff20f
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-09-11 04:55:35 +0500

    This is my 13th class of JS

---
+++ Class-13/app.js
@@ -0,0 +1,31 @@
+//? WHILE LOOP
+
+// let i = 0;
+// while(i <= 10){
+//     console.log(i)
+//     i++
+// }
+
+// let arr = ["Mango" , "Grapes" , "Banana" , "Pea" , "Apple"]
+// let j = 0;
+// while (j < arr.length){
+//     console.log(arr[i])
+//     j++
+// }
+
+// let k = 0;
+// do {
+//     console.log(k);
+    
+//    k++;
+// } while (k <= 10);
+
+//! But in Do while, if condition is not matched it runs one time atleast like this!!
+
+
+// let l = 0;
+// do {
+//     console.log(l);
+    
+//    l++;
+// } while (l > 10);

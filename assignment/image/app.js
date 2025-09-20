b96a0a9d7e3dbd08b01d6698114dd0e9c8fba1ec
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-09-21 03:31:15 +0500

    This is my JS assignment

---
+++ assignment/image/app.js
@@ -0,0 +1,10 @@
+//! image
+
+var img = document.getElementById("image");
+function imageChange() {
+    img.src = "https://static.vecteezy.com/system/resources/thumbnails/039/545/117/small/ai-generated-beautiful-rose-flowers-with-butterflies-flying-on-them-video.jpg";
+};
+
+function imageReturn() {
+    img.src = "https://st.depositphotos.com/2001755/3622/i/450/depositphotos_36220949-stock-photo-beautiful-landscape.jpg";
+};

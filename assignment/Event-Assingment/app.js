338321928418f1e19c83401f8a275f919ea902e0
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-09-24 05:40:15 +0500

    This is my JS assignment

---
+++ assignment/Event-Assingment/app.js
@@ -0,0 +1,20 @@
+// Random Color Generator
+function colorGenerator(){
+    let color = "#"
+    let colorNo = "0123456789abcdef"
+    for( i = 0 ; i < 6 ; i++){
+        let colorGenerate = colorNo[Math.floor(Math.random() * 16)];
+        color += colorGenerate
+
+
+    } 
+    return color
+}
+function onover(imoji){
+    imojis["🌹 Rose" , "🤣Laugh" , "💕Love" , "🤞Crossed Fingers" ,  "👍Like" , "😎Smiling Face" , "❤White Heart"]
+
+    imoji.style.transition = 
+
+}
+
+

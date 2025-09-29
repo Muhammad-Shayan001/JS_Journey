8a15ba512287ffbd53db0ddd93e3c0711bb7774a
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-09-29 12:54:17 +0500

    This is my DOM 1st Project

---
+++ DOM/Projects/Project-1/app.js
@@ -0,0 +1,16 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+// console.log(button);
+buttons.forEach((button) => {
+    // console.log(button);
+
+    button.addEventListener('click' ,(e) =>{
+        // console.log(e.target);
+        
+        body.style.backgroundColor = e.target.id
+        
+    })
+    
+})
+

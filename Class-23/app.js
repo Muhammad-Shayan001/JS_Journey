13b21daa8f0a2a7f4f878f30f8ec736a95e49ff6
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-10-05 05:11:04 +0500

    This is my JS Journey Class

---
+++ Class-23/app.js
@@ -0,0 +1,17 @@
+class Clicked {
+    constructor(){
+        this.library = "React";
+        this.server = "http:localhost:3000"
+
+        document.querySelector('.button').addEventListener('click' , this.handeClick.bind(this))
+
+    }
+    handeClick(){
+        console.log("Button Clicked");
+        console.log(this.server);
+        
+        
+    }
+
+}
+const app = new Clicked()

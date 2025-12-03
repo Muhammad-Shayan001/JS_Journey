f71e7a84ba0cd69c441919e3a3eee77859b5efbd
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-12-04 03:18:59 +0500

    This is my 11th class of JS

---
+++ Class-11/app.js
@@ -0,0 +1,26 @@
+// ====================== FOR LOOP ===========
+for(let i = 0 ; i <= 10 ; i++ ){
+    console.log("Ali")
+}
+
+// TALBE
+let table0 = 9
+
+for(let i = 1 ; i <=  10 ; i++){
+    console.log(`${table0} X ${i} = ${table0*i}`)
+
+ }
+let table = +prompt("Enter table number")
+
+for(let i = 1 ; i <=  10 ; i++){
+    console.log(`${table} X ${i} = ${table*i}`)
+
+}
+
+
+
+// print sorry
+
+for(let a = 0 ; a <= 100 ; a++){
+    console.log("SORRY")
+}

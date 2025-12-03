eb1f42040b95c45d897b5b9b327a91ece75dde7e
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-12-04 03:17:44 +0500

    This is my 6th class of JS

---
+++ Class-6/app.js
@@ -0,0 +1,30 @@
+// ==========OPERATORS======
+// LessThan
+let x = 1;
+let y = 2;
+console.log(x < y);
+console.log(y < x);
+
+//Greater Than
+console.log(x > y);
+console.log(y > x);
+//Equal
+console.log(x == y);
+
+//Greater Than   equal
+
+console.log(x >= y);
+console.log(y >= x);
+
+//Less Than   equal
+
+
+console.log(x <= y);
+console.log(y <= x);
+
+//SOLVE IT
+
+let a = 3;
+let b = 5;
+console.log(x + y == y + x - a <= b + b == a >= b == 10 > b ) 
+

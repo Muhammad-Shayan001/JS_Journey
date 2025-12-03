4e5e29555282b677cf42265ada2e57e83ea2865e
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-12-04 03:18:29 +0500

    This is my 8th class of JS

---
+++ Class-8/app.js
@@ -0,0 +1,30 @@
+// // ==========Array Elements===========
+
+let peoples = ["waqas", "sara", "ali", "tahiya", "muhtashim", "alisha", "shayan", "fatima", "umar", a];
+var a = "maryam";
+console.log(peoples);
+let imp1 = peoples.splice(6, 2, );
+console.log(imp1);
+
+let imp2 = peoples.splice(2, 2,);
+
+console.log(imp2);
+let imp3 = peoples.splice(2, 2,);
+console.log(peoples);
+console.log(imp3);
+let imp4 = peoples.splice(2, 2,);
+console.log(imp4);
+console.log(peoples);
+let humans = ["waqas", "sara", "ali", "tahiya", "muhtashim", "alisha", "shayan", "fatima", "umar", a];
+var a = "maryam";
+console.log(humans)
+let couple1 = humans.slice(2 , 4)
+console.log(couple1);
+let couple2 = humans.slice(4 , 6)
+console.log(couple2);
+let couple3 = humans.slice(6 , 8)
+console.log(couple3);
+let couple4 = humans.slice(8 , 10)
+console.log(couple4);
+
+
